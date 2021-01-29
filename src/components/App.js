4c8216a6727d17ccffd38bdea526ea4e253ac856
Gabrielle Smith <74670296+smithgabs@users.noreply.github.com>
2021-01-29 13:50:11 -0500

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-template-curly-in-string */
+
+import './App.css';
+import React from 'react';
+import BusinessList from './BusinessList/BusinessList';
+import SearchBar from './SearchBar/SearchBar';
+import Yelp from '../util/Yelp.js';
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      businesses: []
+    };
+
+    this.searchYelp = this.searchYelp.bind(this);
+  } 
+  searchYelp(term, location, sortBy) {
+    Yelp.searchYelp(term, location, sortBy).then(businesses => {
+      this.setState({businesses: businesses});
+    });
+  }
+  render() {
+   return(
+    <div className="App">
+      <h1>ravenous</h1>
+      <SearchBar searchYelp={this.searchYelp}/>
+      <BusinessList businesses={this.state.businesses} />
+   </div>
+  );
+}
+}
+
+export default App;
